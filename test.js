43d65d3df5e5682aaaed6544756e4f2b815fad56
Radek <radekhendriksz@gmail.com>
2024-07-04 13:04:16 +0200

    first commit

---
+++ test.js
@@ -0,0 +1,25 @@
+var Singleton = (function () {
+    var instance;
+
+    function createInstance() {
+        var object = new Object("I was instantiated at: RIGHT NOW ");
+
+        return object;
+    }
+
+    return {
+        getInstance: function () {
+            if (!instance) {
+                instance = createInstance();
+            }
+
+            return instance;
+        }
+    };
+})();
+
+var instance1 = Singleton.getInstance();
+var instance2 = Singleton.getInstance();
+
+console.log(instance1);
+console.log(instance2);
